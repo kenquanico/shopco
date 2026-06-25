f568dde320f483e932ceaaa8fa110e722d3b6cd2
kenquan <nekquanico@gmail.com>
2026-06-25 23:32:59 +0800

    add HomeView component for showcasing products and customer testimonials

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import HomeView from './views/HomeView.vue'
+import CategoryView from './views/CategoryView.vue'
+import ProductView from './views/ProductView.vue'
+import CartView from './views/CartView.vue'
+import CheckoutView from './views/CheckoutView.vue'
+import SearchView from './views/SearchView.vue'
+import './style.css'
+
+const router = createRouter({
+  history: createWebHistory(),
+  scrollBehavior: () => ({ top: 0 }),
+  routes: [
+    { path: '/', component: HomeView },
+    { path: '/category/:slug', component: CategoryView },
+    { path: '/product/:id', component: ProductView },
+    { path: '/cart', component: CartView },
+    { path: '/checkout', component: CheckoutView },
+    { path: '/search', component: SearchView },
+  ],
+})
+
+createApp(App).use(router).mount('#app')
